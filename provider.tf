20dd4b80a62f4435a621ab844f48956cc44c26a1
nicolas-gagnon <nicolasgagnon4@hotmail.com>
2025-04-23 16:02:43 -0400

    update

---
+++ provider.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "nico-s3-example-bucket-123456"
+
+  tags = {
+    Name        = "Example S3 Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
